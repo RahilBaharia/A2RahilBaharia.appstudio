b1634709c015a7291c9ce4398e47659d5ac126c5
RahilBaharia <79479601+RahilBaharia@users.noreply.github.com>
2021-02-28 15:27:16 -0600

    added comments, inserted messages into an array, and started the if else statements

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,3 +1,22 @@
+//get users first name
 let firstName = prompt("What is your first name?")
+console.log(firstName)
+
+//get users state
 let userState = prompt("What state do you live in? (Ex: NE)")
+console.log(userState)
+
+//get users temperature
 let userTemperature = prompt("What is the current outside temperature? (Ex: 55)")
+console.log(userTemperature)
+
+//Put users info in array
+let array = [firstName, userState, userTemperature]
+console.log(array)
+
+//put messages into array
+let messages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you won't need a hat, scarf or gloves.", "wear your warmest coat, a warm hat, a scarf, and warm gloves.", "wear a warm coat, hat and gloves. Maybe a scarf too."]
+
+//if else statement about users state and weather
+if (usersState = "NE" && userTemperature >= 32)
+    console.log(message[0])
