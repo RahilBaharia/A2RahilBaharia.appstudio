f3b0d4b3f660cecb17f5fc525fa2029e9bb398a9
RahilBaharia <79479601+RahilBaharia@users.noreply.github.com>
2021-02-28 15:13:40 -0600

    Inserted users first name, state, and temperature

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,3 @@
+let firstName = prompt("What is your first name?")
+let userState = prompt("What state do you live in? (Ex: NE)")
+let userTemperature = prompt("What is the current outside temperature? (Ex: 55)")
